fc94034ff415bb1fb989489f51d83c4f9f162ae8
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-25 01:26:59 +0700

    Update leave.py

--- SpamX/modules/for_bot/leave.py
+++ SpamX/modules/for_bot/leave.py
@@ -23,27 +23,27 @@ async def leave(SpamX: Client, e: Message):
     if len(e.text) > 7:
         chat = rizoel[0]
         try:
-           if chat in [-1001321613309, 1321613309, "@DNHxHELL"]:
+           if chat in [-1002025317318, 6337933296, "@coihaycoc"]:
               return
            await SpamX.leave_chat(chat)
-           await e.reply_text("**Left Successfully ✅ **")
+           await e.reply_text("**Rời đi thành công ✅ **")
         except Exception as ex:
            await e.reply_text(leave_errors(ex))
     else:
         chat = e.chat.id
         ok = e.from_user.id
         if int(chat) == int(ok):
-            return await e.reply_text(f"Usage: {handler}leave <chat username or id> or {handler}leave (type in Group for Direct leave)")
-        if int(chat) == -1001321613309:
+            return await e.reply_text(f"Cách sử dụng: {handler}leave <tên người dùng hoặc id trò chuyện> hoặc {handler}leave (nhập trong Nhóm muốn rời đi trực tiếp)")
+        if int(chat) == -1002025317318:
               return
         try:
            await SpamX.leave_chat(chat)
-           await e.reply_text("**Left Successfully ✅ **")
+           await e.reply_text("**Rời đi thành công ✅ **")
         except Exception as ex:
            await e.reply_text(leave_errors(ex))
         if LOGS_CHANNEL:
            try:
-                await SpamX.send_message(LOGS_CHANNEL, f"Leaved Chat \n\n Chat: {chat} \n Cmd By User: {e.from_user.id}")
+                await SpamX.send_message(LOGS_CHANNEL, f"Rời khỏi cuộc trò chuyện \n\n Trò chuyện: {chat} \n Cmd theo người dùng: {e.from_user.id}")
            except Exception as a:
              print(a)
              pass
