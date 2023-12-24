ff85f0c4a2b72b1e3e2b5520b6b96eac24967567
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-25 02:14:02 +0700

    Update msgall.py

--- SpamX/modules/for_id/msgall.py
+++ SpamX/modules/for_id/msgall.py
@@ -18,18 +18,18 @@ async def msgall(SpamX: Client, message: Message):
     elif message.reply_to_message:
        msg = message.reply_to_message.text.markdown
     else:
-       await message.reply_text("Give Message!")
+       await message.reply_text("Gửi tin nhắn!")
        return
     chat = message.chat
     user = message.from_user
     if chat.id == user.ud:
-       """ Can't Use this Cmd in PM """
-       await message.reply_text("Use This Cmd in group")
+       """ Không thể sử dụng Cmd này trong PM """
+       await message.reply_text("Sử dụng Cmd này trong nhóm")
        return
     if int(chat.id) in res_grps:
-       await message.reply_text("**Sorry !! You can't use this cmd in this Group-!**")
+       await message.reply_text("**Lấy làm tiếc !! Bạn không thể sử dụng cmd này trong Nhóm này-!**")
        return
-    Sah = await message.reply_text("__Sending Message to all group members__")
+    Sah = await message.reply_text("__Gửi tin nhắn cho tất cả thành viên nhóm__")
     done = 0
     fail = 0
     async for x in SpamX.get_chat_members(chat):
@@ -41,5 +41,5 @@ async def msgall(SpamX: Client, message: Message):
        except Exception as a:
           fail += 1
           print(a)
-    await SpamX.send_message(user.id, f"Messaged all group members! \n\nsent to `{done}` users \nfailed: `{fail}`")
+    await SpamX.send_message(user.id, f"Đã nhắn tin cho tất cả thành viên trong nhóm! \n\ngửi đến `{done}` users \nthất bại: `{fail}`")
     await sab.delete()
