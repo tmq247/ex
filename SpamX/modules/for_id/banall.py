b5527eb1002d5fb3a86dc4d95f7414b9b3c49bb3
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-25 01:46:38 +0700

    Update banall.py

--- SpamX/modules/for_id/banall.py
+++ SpamX/modules/for_id/banall.py
@@ -8,6 +8,6 @@ from RiZoeLX.functions import start_banall
 @Client.on_message(filters.me & filters.command(["banall", "fuckall"], prefixes=handler))
 async def banall(SpamX: Client, message: Message):
    if message.chat.id == message.from_user.id:
-     await message.reply_text("Use this CMD in group;")
+     await message.reply_text("Sử dụng CMD này trong nhóm;")
      return
    await start_banall(SpamX, message)
