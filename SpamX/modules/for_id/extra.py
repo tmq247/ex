3f8ff90c993b231fe50fd0910cb758e2a84bbc26
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-25 02:05:51 +0700

    Update extra.py

--- SpamX/modules/for_id/extra.py
+++ SpamX/modules/for_id/extra.py
@@ -17,14 +17,14 @@ async def uspam(SpamX: Client, e: Message):
     global unlimited
     unlimited = True
     if int(e.chat.id) in res_grps:
-       await e.reply_text("**Sorry !! i Can't Spam Here.**")
+       await e.reply_text("**Lấy làm tiếc !! tôi không thể spam ở đây.**")
        return
     msg = str(e.text[6:]) 
     if not msg:
        await e.reply("Gime Spam message bruh!")
        return
     if re.search(res_devs.lower(), msg.lower()):
-       await e.reply("**Sorry !!** I can't Spam On @RiZoeLX's owner")
+       await e.reply("**Xin lỗi !!** Tôi không thể Spam trên chủ sở hữu @coihaycoc")
        return
 
     try:
@@ -32,11 +32,11 @@ async def uspam(SpamX: Client, e: Message):
            await SpamX.send_message(e.chat.id, msg)
     except Exception as ex:
            print(ex)
-           await e.reply_text(f" Error -! \n\n {ex}")
+           await e.reply_text(f" Lỗi -! \n\n {ex}")
            
     if LOGS_CHANNEL:
          try:
-            await SpamX.send_message(LOGS_CHANNEL, f"started Unlimited Spam By User: {e.from_user.id} \n\n Chat: {e.chat.id} \n Spam Message: {msg}")
+            await SpamX.send_message(LOGS_CHANNEL, f"đã bắt đầu Thư rác không giới hạn theo người dùng: {e.from_user.id} \n\n Trò chuyện: {e.chat.id} \n Tin nhắn rác: {msg}")
          except Exception as a:
              print(a)
              pass
@@ -61,7 +61,7 @@ async def uraid(SpamX: Client, e: Message):
 
       if LOGS_CHANNEL:
          try:
-            await SpamX.send_message(LOGS_CHANNEL, f"started Raid By User: {e.from_user.id} \n\n On User: {mention} \n Chat: {e.chat.id}")
+            await SpamX.send_message(LOGS_CHANNEL, f"đã bắt đầu Đột kích theo người dùng: {e.from_user.id} \n\n Trên người dùng: {mention} \n Trò chuyện: {e.chat.id}")
          except Exception as a:
              print(a)
              pass
@@ -74,7 +74,7 @@ async def abuse(SpamX: Client, e: Message):
      if sex:
           counts = int(sex)
           if int(e.chat.id) in res_grps:
-              return await e.reply_text("**Sorry !! i Can't Spam Here.**")
+              return await e.reply_text("**Lấy làm tiếc !! tôi không thể spam ở đây.**")
           for _ in range(counts):
               msg = choice(one_word)
               await SpamX.send_message(e.chat.id, msg)
@@ -83,14 +83,14 @@ async def abuse(SpamX: Client, e: Message):
           global unlimited
           unlimited = True
           if int(e.chat.id) in res_grps:
-               return await e.reply_text("**Sorry !! i Can't Spam Here.**")
+               return await e.reply_text("**Lấy làm tiếc !! tôi không thể spam ở đây.**")
           try:
              while unlimited == True:
                  msg = choice(one_word)
                  await SpamX.send_message(e.chat.id, msg)
           except Exception as ex:
               print(ex)
-              await e.reply_text(f" Error -! \n\n {ex}")
+              await e.reply_text(f" Lỗi -! \n\n {ex}")
 
 
 @Client.on_message(filters.user(Sudos) & filters.command(["stop"], prefixes=handler))
@@ -98,7 +98,7 @@ async def abuse(SpamX: Client, e: Message):
 async def stop(_, e: Message):
        global unlimited
        unlimited = False
-       await e.reply_text("Stopped Unlimited Spam/Raid/abuse -;")
+       await e.reply_text("Đã dừng không giới hạn Spam/Raid/abuse -;")
 
 @Client.on_message(filters.user(Sudos) & filters.command(["echo", "repeat"], prefixes=handler))
 @Client.on_message(filters.me & filters.command(["echo", "repeat"], prefixes=handler))
@@ -109,7 +109,7 @@ async def echo_(SpamX: Client, message: Message):
     elif txt:
       msg = str(txt)
     else:
-        await message.reply_text(f"**Wrong Usage!** \n\n Syntax: {handler}echo (message or reply to message)")
+        await message.reply_text(f"**Sử dụng sai!** \n\n Cú pháp: {handler}echo (nhắn tin hoặc trả lời tin nhắn)")
         return
 
     try:
@@ -132,13 +132,13 @@ async def Red7_report(SpamX: Client, message: Message):
       try:
          suser = await SpamX.get_users(args[0])
       except Exception as a:
-         await message.reply_text(f"**ERROR:** {a}")
+         await message.reply_text(f"**LỖI:** {a}")
          return
       if int(suser.id) in Devs:
-         await y.reply_text("User is Dev of SpamX!")
+         await y.reply_text("Người dùng là Dev của SpamX!")
          return
       elif int(suser.id) == Owner:
-         await message.reply_text("User is owner of these bots!")
+         await message.reply_text("Người dùng là chủ sở hữu của các bot này!")
          return
       else:
          if suser.username:
@@ -149,7 +149,7 @@ async def Red7_report(SpamX: Client, message: Message):
          if re.search(R7_ban_codes.lower(), reason.lower()):
             report_reason = reason
          else:   
-            await message.reply_text("**Error:** Wrong reason Code \n\n [Click Here.](https://t.me/Red_Seven_Network/14) to get all reason codes!", disable_web_page_preview=True)
+            await message.reply_text("**Lỗi:** Mã lý do sai \n\n [Bấm vào đây.](https://t.me/moimoimusicbot) để có được tất cả các mã lý do!", disable_web_page_preview=True)
             return
          proof = str(txt[1])
          if proof.startswith("https://telegra.ph/file") or proof.startswith("https://telegra.ph"):
@@ -158,15 +158,15 @@ async def Red7_report(SpamX: Client, message: Message):
             await message.reply_text("need single telegraph link as a proof!")
             return
       try:
-         await SpamX.unblock_user("Red7Systembot")
+         await SpamX.unblock_user("muoimuoimusicbot")
          Report_message = f"/report {report_user} {report_reason} {report_proof}"
-         await SpamX.send_message("Red7Systembot", Report_message)
+         await SpamX.send_message("muoimuoimusicbot", Report_message)
          await asyncio.sleep(2) 
-         async for response in SpamX.get_chat_history("Red7Systembot", 1):
-            hm = await SpamX.forward_messages(chat.id, "Red7Systembot", response.id)
-            await hm.reply_text(f"{user.mention}! Check")
+         async for response in SpamX.get_chat_history("muoimuoimusicbot", 1):
+            hm = await SpamX.forward_messages(chat.id, "muoimuoimusicbot", response.id)
+            await hm.reply_text(f"{user.mention}! Kiểm tra")
       except Exception as eror:
          await message.reply_text(str(eror))
 
    else:
-       await message.reply_text(f"**Wrong Usage!** \n\n Syntax: {handler}report (user) (reason code ([get here.](https://t.me/Red_Seven_Network/14)) (single proof telegraph link only)", disable_web_page_preview=True)
+       await message.reply_text(f"**Cách sử dụng sai!** \n\n Cú pháp: {handler}report (user) (nguyên mã ([đến đây.](https://t.me/moimoimusicbot)) (single proof telegraph link only)", disable_web_page_preview=True)
